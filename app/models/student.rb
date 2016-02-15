e4d31c082717647eff5b1c3419a4a71e825a3a16
Phil Thomas <pthomas551@gmail.com>
2016-02-15 17:35:00 -0600

    Add student model and view

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,9 @@
 class Student < ActiveRecord::Base
+  belongs_to :user
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :grade
+  validates_presence_of :gpa
+  validates_presence_of :detentions
+  validates_presence_of :gender
 end
