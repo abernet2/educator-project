b62d8d44374e03431dba31cb78ce858ea68ce22a
tommchenry <tommchenry@gmail.com>
2016-02-15 17:02:35 -0600

    Updates seed file to include username.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 require 'faker'
 
 30.times do
-	User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "test", photo_url: Faker::Avatar.image)
+	User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Internet.user_name, password: "test", photo_url: Faker::Avatar.image)
 end
