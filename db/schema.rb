ebf442b45dd73b88a97a9706fdca9049e5795180
Jack Abernethy <email": null>
2016-02-15 15:45:13 -0600

    Add migration for password_digest attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215212402) do
+ActiveRecord::Schema.define(version: 20160215214111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,12 +31,13 @@ ActiveRecord::Schema.define(version: 20160215212402) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name", null: false
-    t.string   "last_name",  null: false
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
     t.string   "photo_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "username",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
