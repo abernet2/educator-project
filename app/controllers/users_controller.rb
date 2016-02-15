8a568d68bf9db4d8dd8524e942b03584fd6d3792
pdynowski <paul.dynowski@gmail.com>
2016-02-15 16:02:12 -0600

    Work on users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       @students = @user.students
     else
       @error = "Unauthenticated user"
-      redirect_to root_path
+      redirect_to new_session_path
     end
   end
 
