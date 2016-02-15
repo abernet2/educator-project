1f4e7ba6abd9976d51f00d6c962877ef5b5d7fa2
Phil Thomas <pthomas551@gmail.com>
2016-02-15 15:27:36 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, except: [:edit, :update, :destroy] do
+    resources :students
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root '/sessions/new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
