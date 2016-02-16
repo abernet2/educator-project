6ad9ce353a353902ea4a31d3bf6192d465534368
Phil Thomas <pthomas551@gmail.com>
2016-02-16 10:30:29 -0600

    Fix trailing whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :users, except: [:edit, :update, :destroy]
   resources :students, only: [:index, :show, :update]
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
