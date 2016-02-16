7cca513e924747208c38c2ef472a62399b6dc6f4
pdynowski <paul.dynowski@gmail.com>
2016-02-15 18:13:07 -0600

    Add teacher page view, sans students, which don't exist in the db yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def show
     if(session[:user_id])
       @user = User.find(params[:id])
-      # @students = @user.students
+      @students = @user.students
     else
       @error = "Unauthenticated user"
       redirect_to new_session_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 has_secure_password
-# has_many :students, foreign_key: :teacher_id
+has_many :students, foreign_key: :teacher_id
 
   def full_name
     "#{self.first_name} #{self.last_name}"
