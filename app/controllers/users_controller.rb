1277bc7f999321c484b113e2e3965a919a18d2d3
Jack Abernethy <jabber26@gmail.com>
2016-02-15 20:41:16 -0600

    Add registration ability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :first_name, :last_name)
+  end
+
 end
