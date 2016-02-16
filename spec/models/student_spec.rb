73616638fbb0d53a7deb2ca5ca453a27ec368a84
Jack Abernethy <jabber26@gmail.com>
2016-02-15 20:45:55 -0600

    Fix student validation to check for belongs_to teacher instead of user

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'shoulda-matchers'
 RSpec.describe Student, type: :model do
-  it { should belong_to :user }
+  it { should belong_to :teacher }
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :grade }
