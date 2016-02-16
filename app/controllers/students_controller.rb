f391def34cec1b89de291a8510ed9cd8a22712a8
tommchenry <tommchenry@gmail.com>
2016-02-15 18:57:07 -0600

    Add index view of all students.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,11 @@
 class StudentsController < ApplicationController
 
-def index
-  @students = Student.all
-end
+  def index
+    @students = Student.all
+  end
 
-def show
-  @student = Student.find(params[:id])
-end
+  def show
+    @student = Student.find(params[:id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'sessions#new'
-
+  get 'students/' => 'students#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
