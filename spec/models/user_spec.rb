2bf86dfdf690bec6b530eeae814ebc1bc45fac35
Jack Abernethy <email": null>
2016-02-17 13:20:55 -0600

    Add test of user.full_name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-describe User do
+RSpec.describe User, type: :model do
   it {should have_many :students}
   it {should validate_presence_of :first_name}
   it {should validate_presence_of :last_name}
