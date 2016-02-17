e1d7006c2411e96c0fcb75aed5a17ea7fdca2d04
tommchenry <tommchenry@gmail.com>
2016-02-17 11:11:33 -0600

    Adds JS sorting for all fields.

--- app/assets/javascripts/sort_columns.js
+++ app/assets/javascripts/sort_columns.js
@@ -1,14 +1,33 @@
 $(document).ready(function(){
 	var students = $('.student-row');
 
-	$('.sort_link').on('click', function(event){
+	$('.number-sort').on('click', function(event){
 		event.preventDefault();
-		var columnHead = $(this);
-		var sortColumn = $(this).parent().attr('id');
+		var columnHead = $(this).find('.sort_link');
+		var sortColumn = $(this).attr('id');
 		if (columnHead.hasClass('sort-desc') == false){
 			student_desc(students, sortColumn, columnHead);
 		} else {
 			student_asc(students, sortColumn, columnHead);
+		}
+	});
+
+	$('.name-sort').on('click', function(event){
+		event.preventDefault();
+		var columnHead = $(this).find('.sort_link');
+		var sortColumn = $(this).attr('id');
+		if (columnHead.hasClass('sort-desc') == false){
+			$('.sort-desc').removeClass('sort-desc');
+			$('.sort-asc').removeClass('sort-asc');
+			students = sort_asc(students, sortColumn);
+			students.detach().appendTo('.table');
+			columnHead.addClass("sort-desc");
+		} else {
+			$('.sort-desc').removeClass('sort-desc');
+			$('.sort-asc').removeClass('sort-asc');
+			students = sort_desc(students, sortColumn);
+			students.detach().appendTo('.table');
+			columnHead.addClass("sort-asc");
 		}
 	});
 
@@ -32,7 +51,11 @@ $(document).ready(function(){
 		sorted = students.sort(function(a,b) {
 			var c = $('#' + a.getAttribute('id') + ' .' + sortColumn).text(),
 				d = $('#' + b.getAttribute('id') + ' .' + sortColumn).text();
-			return d - c;
+			if(c > d) { 
+				return -1;
+			} else {
+				return 1;
+			}
 		});
 		return sorted
 	}
@@ -41,19 +64,14 @@ $(document).ready(function(){
 		sorted = students.sort(function(a,b) {
 			var c = $('#' + a.getAttribute('id') + ' .' + sortColumn).text(),
 				d = $('#' + b.getAttribute('id') + ' .' + sortColumn).text();
-			return c - d;
+			if(c < d) { 
+				return -1;
+			} else {
+				return 1;
+			}
+
 		});
 		return sorted
 	}
-
-	// $('#name-sort').on('click', function(event){
-	// 	event.preventDefault();
-	// 	students.sort(function(a,b) {
-	// 		var c = $('#' + a.getAttribute('id') + ' .student-name').text(),
-	// 			d = $('#' + b.getAttribute('id') + ' .student-name').text();
-	// 		return d - c;
-	// 	});
-	// 	students.detach().appendTo('.table');
-	// });
 
 });
