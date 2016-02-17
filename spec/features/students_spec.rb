5d808d4ba9084926507aa7d00c4caeffcc19dd76
pdynowski <paul.dynowski@gmail.com>
2016-02-16 23:35:31 -0600

    Work on tests

--- spec/features/students_spec.rb
+++ spec/features/students_spec.rb
@@ -1,31 +1,32 @@
-require 'rails_helper'
-
-feature "User visiting student page" do
+describe "the students index page", type: :feature do
   before :each do
     user = build(:user)
     user.username = 'admin'
     user.password = 'password'
     user.save
-  end
-  scenario "when user visits student page while logged in" do
     visit '/'
     within("#login-form") do
       fill_in 'username', with: "admin"
       fill_in 'password', with: "password"
     end
     click_button 'Login'
+  end
+
+  scenario "when user visits student page while logged in" do
     student = build(:student)
     student.teacher_id = user.id
     student.save
-    visit '/students/1'
+    visit "/students/#{student.id}"
     expect(page).to have_content('GPA')
+    expect(page).to have_content("#{student.full_name}")
   end
 
   scenario "when user visits student page while not logged in" do
+    click_link "Log out"
     student = build(:student)
     student.teacher_id = user.id
     student.save
     visit '/students/1'
-    expect(page).to have_content('GPA')
+    expect(page).to have_content('Login')
   end
 end
