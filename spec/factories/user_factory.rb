daf596b7887c4a8e833a3b69c2b698fb5d75e747
Jack Abernethy <email": null>
2016-02-17 08:21:55 -0600

    Add confirmation code to user factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,8 +1,9 @@
 FactoryGirl.define do
-  factory :user do 
+  factory :user do
     first_name "John"
     last_name "Doe"
     username "User_name"
     password "Password"
+    authentication_token ENV['USER_REGISTRATION_TOKEN']
   end
 end
