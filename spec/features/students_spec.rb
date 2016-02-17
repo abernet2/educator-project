36396e28b4ea17a177b7e086f384e71c8eecfdc5
pdynowski <paul.dynowski@gmail.com>
2016-02-16 19:16:28 -0600

    Fix failing tests, I hope

--- spec/features/students_spec.rb
+++ spec/features/students_spec.rb
@@ -1,9 +1,27 @@
 require 'rails_helper'
 
 feature "User visiting student page" do
-  scenario "when user visits student page" do
+  before :each do
     user = build(:user)
+    user.username = 'admin'
+    user.password = 'password'
     user.save
+  end
+  scenario "when user visits student page while logged in" do
+    visit '/'
+    within("#login-form") do
+      fill_in 'username', with: "admin"
+      fill_in 'password', with: "password"
+    end
+    click_button 'Login'
+    student = build(:student)
+    student.teacher_id = user.id
+    student.save
+    visit '/students/1'
+    expect(page).to have_content('GPA')
+  end
+
+  scenario "when user visits student page while not logged in" do
     student = build(:student)
     student.teacher_id = user.id
     student.save
